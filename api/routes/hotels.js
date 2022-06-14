d95f53e1d89e12c0c06972f4e114ba8420b004a3
Nikola Chakarov <chakarovn1975@gmail.com>
2022-06-14 15:17:26 +0300

    end hotels routes

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -16,7 +16,55 @@ router.post("/", async (req, res) => {
 });
 
 // UPDATE
+router.put("/:id", async (req, res) => {
+	try {
+		const updatedHotel = await Hotel.findByIdAndUpdate(
+			req.params.id,
+			{ $set: req.body },
+			{ new: true }
+		);
+
+		res.status(200).json({ status: "success", data: updatedHotel });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ status: "fail", message: err.message });
+	}
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+	try {
+		const deletedHotes = await Hotel.findByIdAndDelete(req.params.id);
+
+		res.status(200).json({ status: "success", data: deletedHotes });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ status: "fail", message: err.message });
+	}
+});
+
 // GET
+router.get("/:id", async (req, res) => {
+	try {
+		const hotel = await Hotel.findById(req.params.id);
+
+		res.status(200).json({ status: "success", data: hotel });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ status: "fail", message: err.message });
+	}
+});
+
 // GET ALL
+router.get("/", async (req, res) => {
+	try {
+		const hotels = await Hotel.find();
+
+		res.status(200).json({ status: "success", data: hotels });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ status: "fail", message: err.message });
+	}
+});
 
 module.exports = router;
